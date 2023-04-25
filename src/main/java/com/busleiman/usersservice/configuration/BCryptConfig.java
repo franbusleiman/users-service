057ba567c67630c2af69e79089c00855c07ca190
franbusleiman <franbusleiman@gmail.com>
2023-04-24 21:02:42 -0300

    Adding swagger documentation.

--- src/main/java/com/busleiman/usersservice/configuration/BCryptConfig.java
+++ src/main/java/com/busleiman/usersservice/configuration/BCryptConfig.java
@@ -3,13 +3,28 @@ package com.busleiman.usersservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class BCryptConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.busleiman.usersservice"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
