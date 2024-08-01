62df27a7c8781bf5aaf43035e9f3b55eef828411
franbusleiman <franbusleiman@gmail.com>
2024-08-01 18:56:32 -0300

    c

--- src/main/resources/db.migration/V3_Add_identifier_unique.sql
+++ src/main/resources/db.migration/V3_Add_identifier_unique.sql
@@ -1,1 +1,7 @@
+DELETE FROM users
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM users
+    GROUP BY identificationNr
+);
 ALTER TABLE users ADD CONSTRAINT unique_document UNIQUE (identificationNr);
