a685953b05b7e3baf306724f766095cc8d375eaf
franbusleiman <franbusleiman@gmail.com>
2024-08-03 22:00:21 -0300

    c

--- src/main/resources/db/migration/V3__Add_identifier_unique.sql
+++ src/main/resources/db/migration/V3__Add_identifier_unique.sql
@@ -1,3 +1,14 @@
+-- Delete related rows in users_roles
+DELETE FROM users_roles
+WHERE users_id NOT IN (
+    SELECT id FROM (
+        SELECT MIN(id) AS id
+        FROM users
+        GROUP BY identification_nr
+    ) AS temp_table
+);
+
+-- Delete from users table
 DELETE FROM users
 WHERE id NOT IN (
     SELECT id FROM (
