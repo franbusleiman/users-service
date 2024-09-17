e1d66a9d3caa6f771e2542514d4bd56864dd5fbc
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-09-17 13:32:14 -0300

    Adding UserPreLunch record

--- src/main/java/com/liro/usersservice/services/UserPreLunchServiceImpl.java
+++ src/main/java/com/liro/usersservice/services/UserPreLunchServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserPreLunchServiceImpl implements UserPreLunchService{
 
-    @Autowired
+    @Autowired(required=true)
     UserPreLunch userPreLunch;
 
     @Autowired
